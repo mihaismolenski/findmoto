c33b585d7ec255b7a1aa30cf605619204a3dd722
Mihai Smolenschi <mihaismolenski@gmail.com>
2023-01-18 14:55:17 +0200

    Added modal background

--- src/components/AsideModal.tsx
+++ src/components/AsideModal.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from "react";
 
-import { useHandleClickOutside } from "../hooks/useHandleClickOutside";
-
 export interface AsideModalProps {
     children: any;
     visible: boolean;
@@ -11,16 +9,22 @@ export interface AsideModalProps {
 export const AsideModal = ({ children, visible, handleClose }: AsideModalProps) => {
     const ref = useRef(null);
 
-    useHandleClickOutside(ref, handleClose);
-
     useEffect(() => {
-       document.body.style.overflow = visible ? "hidden" : "unset";
+        document.body.style.overflow = visible ? "hidden" : "unset";
 
     }, [visible])
 
-    return <aside className={`aside-modal ${visible ? "active" : ""}`} ref={ref}>
-        {children}
-    </aside>
+    return (
+        <>
+            <div
+                className={`aside-modal-background ${visible ? "visible" : "hidden"}`}
+                onClick={() => handleClose && handleClose()}
+            ></div>
+            <aside className={`aside-modal ${visible ? "active" : ""}`} ref={ref}>
+                {children}
+            </aside>
+        </>);
+
 };
 
 export default AsideModal;
