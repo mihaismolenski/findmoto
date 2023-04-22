1ee3dc819d0d5eb824d51883110a09529cc56de1
Mihai Smolenschi <mihaismolenski@gmail.com>
2023-04-22 11:49:18 +0300

    Added new manufacturers

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -51,16 +51,29 @@ export const getLogo = (manufacturer: string) => {
       return "../images/manufacturers/benelli.png";
     case "Beta":
       return "../images/manufacturers/beta.png";
+    case "Big Dog":
+      return "../images/manufacturers/bigdog.png";
     case "Bimota":
       return "../images/manufacturers/bimota.svg";
     case "BMW":
       return "../images/manufacturers/bmw.png";
+    case "Boss Hoss":
+      return "../images/manufacturers/boss-hoss.png";
+    case "Buell":
+      return "../images/manufacturers/buell.png";
+    case "California Scooter":
+    case "CSC Motorcycles":
+      return "../images/manufacturers/csc.png";
+    case "Can-Am":
+      return "../images/manufacturers/can-am.png";
     case "CF Moto":
       return "../images/manufacturers/cfmoto.webp";
     case "Cobra":
       return "../images/manufacturers/cobra.png";
     case "Ducati":
       return "../images/manufacturers/ducati.png";
+    case "E-Ton":
+      return "../images/manufacturers/e-ton.png";
     case "GAS GAS":
       return "../images/manufacturers/gasgas.webp";
     case "Harley-Davidson":
@@ -72,34 +85,60 @@ export const getLogo = (manufacturer: string) => {
       return "../images/manufacturers/honda.png";
     case "Husqvarna":
       return "../images/manufacturers/husqvarna.png";
+    case "Hyosung":
+      return "../images/manufacturers/hyosung.webp";
+    case "Ice Bear":
+      return "../images/manufacturers/ice-bear.jpg";
     case "Indian":
       return "../images/manufacturers/indian.png";
+    case "Johnny Pag":
+      return "../images/manufacturers/johnny-pag.webp";
+    case "Kayo":
+      return "../images/manufacturers/kayo.webp";
     case "Kawasaki":
       return "../images/manufacturers/kawasaki.png";
     case "KTM":
       return "../images/manufacturers/ktm.png";
+    case "Kymco":
+      return "../images/manufacturers/kymco.png";
+    case "Lehman":
+      return "../images/manufacturers/lehman.ico";
     case "Moto Guzzi":
       return "../images/manufacturers/motoguzzi.png";
     case "MV Agusta":
       return "../images/manufacturers/mvaugusta.png";
     case "Norton":
       return "../images/manufacturers/norton.png";
+    case "Polaris":
+      return "../images/manufacturers/polaris.png";
     case "Royal":
       return "../images/manufacturers/royal.png";
     case "Sherco":
       return "../images/manufacturers/sherco.png";
     case "Suzuki":
       return "../images/manufacturers/suzuki.webp";
+    case "Sym":
+      return "../images/manufacturers/sym.png";
+    case "Tank Sports":
+      return "../images/manufacturers/tank-sports.avif";
+    case "TGB":
+      return "../images/manufacturers/tgb.png";
     case "TM Racing":
       return "../images/manufacturers/tmracing.png";
     case "Triumph":
       return "../images/manufacturers/triumph.png";
+    case "Ural":
+      return "../images/manufacturers/ural.png";
+    case "Vespa":
+      return "../images/manufacturers/vespa.png";
     case "Victory":
       return "../images/manufacturers/victory.png";
     case "Yamaha":
       return "../images/manufacturers/yamaha.png";
     case "Zero":
       return "../images/manufacturers/zero.png";
+    case "Zongshen":
+      return "../images/manufacturers/zongshen.png";
     default:
       return "-";
   }
